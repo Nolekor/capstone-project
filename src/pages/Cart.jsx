b856d0347afe2f760917a7dbbe5b28fd035ad623
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-04-27 23:10:18 +0200

    add starting logic to cart

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import Image from "../components/Image";
+
+const Cart = () => {
+  const { cartItems } = useContext();
+  return <main className="cart-page">images go here</main>;
+};
+
+export default Cart;
