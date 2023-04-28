3551469fbc73e125c44042642f40098ea2a64724
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-04-28 16:22:43 +0200

    add cart logic

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,35 @@
-import { useContext } from "react";
-import Image from "../components/Image";
+import { useState, useContext } from "react";
+import { Context } from "../Provider";
+import CartItem from "../components/cartItem";
 
 const Cart = () => {
-  const { cartItems } = useContext();
-  return <main className="cart-page">images go here</main>;
+  const [buttonText, setButtonText] = useState("Place Order");
+  const { cartItems, totalCost, emptyCartItems } = useContext(Context);
+  const cartItemsElements = cartItems.map((item) => (
+    <CartItem key={item.id} item={item} />
+  ));
+  function processingOrder() {
+    setButtonText("Processing Order");
+    setTimeout(() => {
+      console.log("Order Placed!"), setButtonText("Place Order");
+      emptyCartItems();
+    }, 3000);
+  }
+
+  return (
+    <main className="cart-page">
+      <h1>Check out</h1>
+      {cartItemsElements}
+      <p className="total-cost">Total: {totalCost(cartItems)}</p>
+      <div className="order-button">
+        {cartItems.length > 0 ? (
+          <button onClick={processingOrder}>{buttonText}</button>
+        ) : (
+          <p>You have no items in the cart!</p>
+        )}
+      </div>
+    </main>
+  );
 };
 
 export default Cart;
